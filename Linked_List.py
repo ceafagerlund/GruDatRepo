6e9398f215b0cdc88910e41703233c5a7df4db43
Alexander_Fagerlund <afage@kth.se>
2019-03-25 16:10:22 +0100

    fungerar kanske

--- Linked_List.py
+++ Linked_List.py
@@ -1,10 +1,20 @@
+
+class ListElement:
+    """Implements a node for linked list use."""
+    def _init_(self,content,next):
+        """Make default node"""
+        self.content = None
+        self.next = None
+
 class LinkedList:
         """This class implements a linked list."""
-        self._first_Element = None                   #first element
-        self._last_Element =  None                   #last element
-        self._length = 0                             #list length
 
-        def _init_(self):
+        def _init_(self,first_Element,last_Element):
+            self._first_Element = None                   #first element
+            self._last_Element =  None                   #last element
+            self._length = 0                             #list length
+
+        def new(self):
             """Construct empty linked list"""
             self._first_Element = ListElement(None,None)
             healthy()
@@ -49,9 +59,6 @@ class LinkedList:
                     iter = iter.next
                 return iter
 
-
-
-
         def removeFirst(specific_list):
             """Remove and returns the first element from this list.
             Return null if the list is empty."""
@@ -63,52 +70,44 @@ class LinkedList:
 
         def clear():
             """Remove all elements from the list."""
-			self._length = 0
+            self._length = 0
 		    #remove?
 
-
-	    def length():
-		    """Return the number of elements in the list."""
-		    return (_self.length)
+        def length():
+            """Return the number of elements in the list."""
+            return (_self.length)
 
         def string():
             """Return a string representation of this list.
             The elements are enclosed in square brackets ("[]").
             Adjacent elements are separated by ", "."""
-        	    iter = _self.first_Element
-			    print("[")
-			    for k = 1:1:_self.length:
-				    print(iter,",")
-				    iter = iter.next
-			    return ("]")
+            iter = _self.first_Element
+            print("[")
+            for k in range(1,self._length):
+                print(iter,",")
+                iter = iter.next
+                return ("]")
 
-class ListElement:
-    """Implements a node for linked list use."""
-    def _init_(self,content,next):
-        """Make default node"""
-        self.content = None
-        self.next = None
+        def healthy(): 		#first, last nollpekare? Vad menas?
+            """Testing function. Checks list."""
+            if _self.length == 0:
+                assert _self.first_Element.content == None 			# empty first, last for empty list
+                assert _self.last_Element.content == None
+                assert _self.first_Element.next == None
+            elif self._length == 1:
+                assert not self._first_Element.content == None and self.last_Element.content == None or self.first_Element.content == None and not self.last_Element.content == None
+            else:
+                assert not self._first_Element.content == None 			# empty first, last for empty list
+                assert not self._last_Element.content == None
+                assert not self._first_Element.next == None
+            assert self._last_Element.next == None 					# always points to null
 
-def healthy(): 		#first, last nollpekare? Vad menas?
-    """Testing function. Checks list."""
-    if _self.length = 0:
-		assert _self.first_Element.content == None 			# empty first, last for empty list
-		assert _self.last_Element.content == None
-		assert _self.first_Element.next == None
-	else if _self.length = 1: 								# either only first or only last
-		assert _self.first_Element.content != None and _self.last_Element.content = None or _self.first_Element.content = None and _self.last_Element.content != None
-	else:
-		assert _self.first_Element.content != None 			# empty first, last for empty list
-		assert _self.last_Element.content != None
-		assert _self.first_Element.next != None
-	assert _self.last_Element.next == None 					# always points to null
-
-	sizecount = 0
-	init = _self.first_Element
-	while init != None 										#assume no None elements in middle of code
-		sizecount += 1
-		init = init.next
-	assert _self.length == sizecount
+            sizecount = 0
+            init = self.first_Element
+            while not init == None: 								
+                sizecount += 1
+                init = init.next
+            assert _self.length == sizecount
 
 
 

e8b65c9ac0599e69f2edd3a4f351b2b915011620
Alexander_Fagerlund <afage@kth.se>
2019-04-09 12:37:03 +0200

    Pseudokod i häfte

--- Ovn3_2.py
+++ Ovn3_2.py
@@ -1,1 +1,7 @@
 # Alexander Fagerlund, grudat19 uppg 3.2, VG-version.
+
+
+def mode(data):
+    """Given array of integers, finds mode. ...
+    if multiple values equally common, returns ...
+    minimum value. Time complexity: O(n)."""

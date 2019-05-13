3f024bd2238f7e0d1d3975bb25a482c22144a2d1
Alexander_Fagerlund <afage@kth.se>
2019-05-13 17:46:58 +0200

    V3

--- FourColorSolver.py
+++ FourColorSolver.py
@@ -10,43 +10,53 @@ def FourColorSolver():
     """Uses __GraphCreator to let user enter own graph,...
     then tries all possible colorings with function
     __Fourcolortrials."""
-    first = __GraphCreator()     #can only handle connected graphs
-    if __FourColorTrials(first) is True:
+    colors = [0,1,2,3]
+    graph = _Graph()
+    graph = __GraphCreator(graph)     #can only handle connected graphs
+    TruthValue = True
+    if __FourColorTrials(graph,colors,TruthValue) is True:
         print("Graph is four-colorable!")
     else:
         print("Graph is NOT four-colorable.")
 
+
 class _Node:
+    """Help class for corners in graph. Not to be tampered with."""
     def __init__(self):
         self._name = None
         self._neighbors = []
+        self._color = None
+        self._isVisited = False
+        self._FailedColors = [False,False,False,False]
+        self._FailCount = 0
 
 class _Graph:
+    """Help class for graph itself. Not to be tampered with."""
     def __init__(self):
         self._first = None
+        self._members = []
+        self._visited = []
 
-def __GraphCreator():
-    """Tool for user to enter chosen corners...
-    and links between corners."""
-    graph = _Graph()
+
+def __GraphCreator(graph):
+    """Tool for user to enter...
+    First: chosen corners...
+    Then: links between said corners."""
     used_node_names = []
-    nodes = []
     ##########################Node names############################
     user_input = input("Provide a node name. Type '"'stop'"' at any time to finish naming nodes.")
     while user_input != "stop":
         if user_input not in used_node_names:
             new = _Node()
             new._name = user_input
-            if not graph._first:
-                graph._first = new
             used_node_names.append(new._name)
-            nodes.append(new)
+            graph._members.append(new)
         else:
             print("Node name already in use. Be more original.")
         user_input = input("Enter new node name")
     print("Graph finished")
     ##########################Node neighbors########################
-    for node in nodes:
+    for node in graph._members:                                  # does not handle linking A to B automatically if B is linked to A
         print("Provide new neighbor for node",node._name,". Type '"'done'"' to move on to next node.")
         inp = input()
         while inp != "done":
@@ -54,19 +64,41 @@ def __GraphCreator():
                 print("That node does not exist... Try again.")
                 inp = input()
             if inp not in node._neighbors:
-                node._neighbors.append(inp)
-            if node._name in node._neighbors:
+                new = _Node()
+                new._name = inp
+                node._neighbors.append(new)
+                new._neighbors.append(node)                      # Undirected graph ==> if A links to B, B has to link to A
+            if inp == node._name:
                 raise ValueError("Not four-colorable as it contains self-loop!")
             print("Enter a new neighbor of",node._name)
             inp = input()
-    return graph._first 
+    print(graph._members)
+    return graph 
 
-def __FourColorTrials(first):
+def __FourColorTrials(graph,colors,TruthValue):                  # Does not work yet...           
     """Examines four-colorability of given graph Graph.
     Returns list of all corners and their colors if...
-    four-colorable, otherwise returns False."""
-    TruthValue = False
-
+    four-colorable, otherwise returns False. Uses DFS search."""
+    for node in graph._members:
+        if node not in graph._visited:
+            print("visiting",node._name)
+            if node._isVisited:
+                return        
+            node._isVisited = True
+            node._color = colors[0]                              # Tries first color
+            for neighbor in node._neighbors:                     # Compare with all neighbors
+                if node._color == neighbor._color:               # If this color assignment failed...
+                    for i in range(0,len(colors)):
+                        if node._color == colors[i]:             # Find which color entry failed this time 
+                            node._FailedColors[i] = True         # Note that that color failed
+                for i in range(0,len(colors)):                   # Examine if all colors failed
+                    if node._FailedColors[i] is True:
+                        node._FailCount += 1                     # See if number of failed assignments is number of available assignments
+                if node._FailCount == len(colors):
+                    TruthValue = False                           # TruthValue is set to "False" as four-coloring fails for this node
+                    break                                        # Can abandon loop, have already seen that four-coloring fails
+            for neighbor in node._neighbors:                     # Recursive call
+                __FourColorTrials(graph,colors,TruthValue)
     return TruthValue
 
 FourColorSolver()
